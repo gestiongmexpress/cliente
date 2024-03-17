bc5ac72b55bb28ee16fdd6e15be08bbabbe56237
diegomorales364 <diego.morales364@gmail.com>
2024-03-17 20:24:15 -0300

    Filtrado de empresas

--- src/app/components/listar-empresas/listar-empresas.component.ts
+++ src/app/components/listar-empresas/listar-empresas.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { EmpresaService } from '../../services/empresa.service';
 import { Empresa } from '../../models/empresa';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-listar-empresas',
@@ -11,17 +12,38 @@ import { Empresa } from '../../models/empresa';
 })
 export class ListarEmpresasComponent implements OnInit {
   empresas: Empresa[] = [];
+  empresasFiltradas: Empresa[] = [];
+  filtroForm: FormGroup;
 
-  constructor(private empresaService: EmpresaService, private router: Router, private toastr: ToastrService) { }
+  constructor(private fb: FormBuilder, private empresaService: EmpresaService, private router: Router, private toastr: ToastrService) {
+    this.filtroForm = this.fb.group({
+      vigente: ['Si'],
+      ordenCompra: [''],
+      sucursal: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.cargarEmpresas();
+    this.filtroForm.valueChanges.subscribe(filtros => {
+      this.aplicarFiltros(filtros);
+    });
+  }
+
+  aplicarFiltros(filtros: any): void {
+    this.empresasFiltradas = this.empresas.filter(empresa => {
+      const vigenteMatch = filtros.vigente ? empresa.vigente === filtros.vigente : true;
+      const ordenCompraMatch = filtros.ordenCompra ? empresa.ordenCompra === filtros.ordenCompra : true;
+      const sucursalMatch = filtros.sucursal ? empresa.sucursal.includes(filtros.sucursal) : true;
+      return vigenteMatch && ordenCompraMatch && sucursalMatch;
+    });
   }
 
   cargarEmpresas(): void {
     this.empresaService.obtenerTodasEmpresas().subscribe({
       next: (data) => {
         this.empresas = data;
+        this.aplicarFiltros(this.filtroForm.value);
       },
       error: (e) => {
         console.error(e);
@@ -29,6 +51,7 @@ export class ListarEmpresasComponent implements OnInit {
       }
     });
   }
+  
 
   editarEmpresa(id: string): void {
     this.router.navigate(['/editar-empresa', id]);
--- src/app/components/listar-tickets/listar-tickets.component.ts
+++ src/app/components/listar-tickets/listar-tickets.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { RegistroTicketService } from '../../services/registroTicket.service';
 import { RegistroTicket } from '../../models/registroTicket';
 import { DateFormatService } from '../../services/date-format.service';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmpresaService } from '../../services/empresa.service';
+import { Empresa } from '../../models/empresa';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-listar-tickets',
   templateUrl: './listar-tickets.component.html',
@@ -11,17 +14,32 @@ import { DateFormatService } from '../../services/date-format.service';
 export class ListarTicketsComponent implements OnInit {
   tickets: RegistroTicket[] = [];
   formatDate = this.dateFormatService.formatDate.bind(this.dateFormatService);
+  ticketsFiltrados: RegistroTicket[] = [];
+  empresas: Empresa[] = [];
+  filtroForm: FormGroup;
 
-  constructor(private registroTicketService: RegistroTicketService, public dateFormatService: DateFormatService) { }
+  constructor(private fb: FormBuilder, private empresaService: EmpresaService, private registroTicketService: RegistroTicketService, public dateFormatService: DateFormatService, private toastr: ToastrService) {
+    this.filtroForm = this.fb.group({
+      empresa: [''],
+      estadoEmpresa: ['Si'],
+      ordenCompra: [''],
+      estadoTicket: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.cargarTickets();
+    this.cargarEmpresas();
+    this.filtroForm.valueChanges.subscribe(filtros => {
+      this.aplicarFiltros(filtros);
+    });
   }
 
   cargarTickets(): void {
     this.registroTicketService.obtenerTodosLosRegistrosTickets().subscribe({
       next: (tickets) => {
         this.tickets = tickets;
+        this.ticketsFiltrados = [...this.tickets];
       },
       error: (e) => console.error(e)
     });
@@ -34,4 +52,66 @@ export class ListarTicketsComponent implements OnInit {
       return ticket.empresa.nombre;
     }
   }
+
+  getEstadoEmpresa(ticket: RegistroTicket): string {
+    if (typeof ticket.empresa === 'object' && ticket.empresa) {
+      return ticket.empresa.vigente === 'Si' ? 'Vigente' : 'No Vigente';
+    } else {
+      return 'Desconocido';
+    }
+  }
+  
+
+  cargarEmpresas(): void {
+    this.empresaService.obtenerTodasEmpresas().subscribe(empresas => {
+      this.empresas = empresas;
+    });
+  }
+
+  aplicarFiltros(filtros: any): void {
+    this.ticketsFiltrados = this.tickets.filter(ticket => {
+      let empresaMatch = true;
+      let estadoEmpresaMatch = true;
+      if (filtros.empresa) {
+        empresaMatch = typeof ticket.empresa === 'string' ?
+          ticket.empresa === filtros.empresa :
+          ticket.empresa._id === filtros.empresa;
+      }
+      if (filtros.estadoEmpresa) {
+        estadoEmpresaMatch = typeof ticket.empresa === 'object' &&
+          (ticket.empresa.vigente === filtros.estadoEmpresa);
+      }
+      const ordenCompraMatch = !filtros.ordenCompra || ticket.ordenCompra === filtros.ordenCompra;
+      const estadoTicketMatch = !filtros.estadoTicket || ticket.estado === filtros.estadoTicket;
+      return empresaMatch && estadoEmpresaMatch && ordenCompraMatch && estadoTicketMatch;
+    });
+  }
+
+  cambiarEstado(ticketId: string, nuevoEstado: string | null): void {
+    if (ticketId === undefined || nuevoEstado === null) {
+      console.error('Ticket ID o nuevo estado es nulo');
+      this.toastr.error('Error al actualizar el estado del ticket. El ID del ticket o el nuevo estado es nulo.', 'Error');
+      return;
+    }
+    this.registroTicketService.actualizarRegistroTicket(ticketId, { estado: nuevoEstado })
+      .subscribe({
+        next: (respuesta) => {
+          console.log('Estado actualizado con éxito', respuesta);
+          this.toastr.success('Estado del ticket actualizado con éxito.', 'Éxito');
+          this.cargarTickets();
+        },
+        error: (error) => {
+          console.error('Error al actualizar el estado del ticket', error);
+          this.toastr.error('Error al actualizar el estado del ticket.', 'Error');
+        }
+      });
+  }
+
+  onSelectEstadoChange(ticketId: string | undefined, event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const nuevoEstado = selectElement.value;
+    if (ticketId) {
+      this.cambiarEstado(ticketId, nuevoEstado);
+    }
+  }  
 }
